60bf53187087741ad1e9e9dcda98b30221499e80
John F. Brown <jack@brownjohnf.com>
2012-10-21 13:42:49 +0000

    added some images; hooked up email

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'sinatra'
 gem 'haml'
+gem 'pony'
 group :production do
   gem 'shotgun'
 end
--- fadidi.rb
+++ fadidi.rb
@@ -1,4 +1,35 @@
 require 'sinatra'
+require 'pony'
+
+configure do
+  Pony.options = {
+    :from => 'info@fadidi.com',
+    :bcc => 'info@fadidi.com',
+    :via => :smtp
+  }
+end
+
+configure :development, :test do
+  Pony.options[:via_options] = {
+    :address => 'smtp.gmail.com',
+    :port => '587',
+    :user_name => 'jfbrown.wa@gmail.com',
+    :password => 'vzLjoaRoBirJgkN',
+    :authentication => :plain,
+    :domain => 'localhost.localdomain'
+  }
+end
+
+configure :production do
+  Pony.options[:via_options] = {
+    :address => ENV['MAILGUN_SMTP_SERVER'],
+    :port => ENV['MAILGUN_SMTP_PORT'],
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
+    :authentication => :plain,
+    :domain => ENV['DOMAIN']
+  }
+end
 
 set :layout => :layout
 
@@ -32,4 +63,13 @@ end
 
 get '/ethos' do
   haml :ethos
+end
+
+get '/newsletter_subscribe' do
+  Pony.mail(
+    :to => params[:subscribe],
+    :from => 'info@fadidi.com',
+    :subject => 'Thanks for your interest!',
+    :body => haml(:subscribe_confirmation, :layout => false)
+  )
 end
